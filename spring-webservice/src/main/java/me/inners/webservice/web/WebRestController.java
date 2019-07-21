e368694c4e947d91e4fcf532966efc681c62318f
minjae <innersme@gmail.com>
2019-07-21 17:37:18 +0900

    Controller 코드 추가

--- spring-webservice/src/main/java/me/inners/webservice/web/WebRestController.java
+++ spring-webservice/src/main/java/me/inners/webservice/web/WebRestController.java
@@ -6,16 +6,20 @@ import me.inners.webservice.domain.posts.PostsRepository;
 import me.inners.webservice.dto.posts.PostsSaveRequestDto;
 
 import me.inners.webservice.service.PostsService;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 @AllArgsConstructor
 public class WebRestController {
 
     private PostsService postsService;
+    private Environment env;
 
     @GetMapping("/hello")
     public String hello(){
@@ -25,5 +29,12 @@ public class WebRestController {
     @PostMapping("/posts")
     public Long savePosts(@RequestBody PostsSaveRequestDto dto){
         return postsService.save(dto);
+    }
+
+    @GetMapping("/profile")
+    public String getProfile(){
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }
